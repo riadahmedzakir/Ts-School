6b5a97b59ecd7ea0466aadf70e840594d4993e9a
riadahmedzakir <33862995+riadahmedzakir@users.noreply.github.com>
2018-01-15 00:41:11 +0600

    Student Add Function

--- class1/dist/bundle.js
+++ class1/dist/bundle.js
@@ -67,7 +67,60 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-throw new Error("Module parse failed: Unexpected token (14:28)\nYou may need an appropriate loader to handle this file type.\n| function studentListDispaly(task) {\r\n|     var newStudentRow = document.createElement(\"tr\");\r\n|     document.getElementById.(\"studentsInfoTable\").appendChild(newStudentRow);\r\n|     var newStudentNameCol = document.createElement(\"td\");\r\n|     var newStudentIdCol = document.createElement(\"td\");\r");
+init();
+function init() {
+    document.getElementById("addStudentButton").addEventListener('click', addStudent);
+    if (localStorage.getItem("studentID") === null) {
+        localStorage.setItem("studentID", "0");
+    }
+    if (localStorage.getItem("savedStudent") != null) {
+        var taskListed = JSON.parse(localStorage.savedStudent);
+        taskListed.forEach(studentListDispaly);
+    }
+}
+function studentListDispaly(studentInfo) {
+    var newStudentRow = document.createElement("tr");
+    var element = document.getElementById("studentsInfoTable");
+    element.appendChild(newStudentRow);
+    var newStudentNameCol = document.createElement("td");
+    var newStudentIdCol = document.createElement("td");
+    var newStudentMarkCol = document.createElement("td");
+    //console.log(studentInfo[0]);
+    var nodeName = document.createTextNode(studentInfo[1]);
+    var nodeId = document.createTextNode(studentInfo[0]);
+    var nodeMark = document.createTextNode(studentInfo[2]);
+    newStudentRow.appendChild(newStudentNameCol);
+    newStudentNameCol.appendChild(nodeName);
+    newStudentRow.appendChild(newStudentIdCol);
+    newStudentIdCol.appendChild(nodeId);
+    newStudentRow.appendChild(newStudentMarkCol);
+    newStudentMarkCol.appendChild(nodeMark);
+}
+function addStudent() {
+    var newStudentName = document.getElementById("studentName").value;
+    var studentMark = document.getElementById("studentMark").value;
+    if (localStorage.getItem("savedStudent") === null) {
+        var newStudent = [
+            [parseInt(localStorage.studentID), newStudentName, studentMark]
+        ];
+        var myJSON = JSON.stringify(newStudent);
+        localStorage.setItem("savedStudent", myJSON);
+        var tempID = parseInt(localStorage.studentID);
+        tempID++;
+        localStorage.setItem("studentID", tempID.toString());
+    }
+    else {
+        var tempID = parseInt(localStorage.studentID);
+        var oldTask = JSON.parse(localStorage.savedStudent);
+        oldTask.push([tempID, newStudentName, studentMark]);
+        var myJSON = JSON.stringify(oldTask);
+        localStorage.setItem("savedStudent", myJSON);
+        tempID++;
+        localStorage.setItem("studentID", tempID.toString());
+    }
+    location.reload();
+}
+
 
 /***/ })
 /******/ ]);
--- class1/src/main.ts
+++ class1/src/main.ts
@@ -7,33 +7,52 @@
          localStorage.setItem("studentID", "0");
      }
 
-     if (localStorage.getItem("savedTask") != null) {
-         let taskListed = JSON.parse(localStorage.savedTask);
+     if (localStorage.getItem("savedStudent") != null) {
+         let taskListed = JSON.parse(localStorage.savedStudent);
          taskListed.forEach(studentListDispaly);
      }
  }
 
- function studentListDispaly(task) {
+ function studentListDispaly(studentInfo:any) {
      let newStudentRow = document.createElement("tr");
-     document.getElementById.("studentsInfoTable").appendChild(newStudentRow);
+     let element = document.getElementById("studentsInfoTable");
+     element.appendChild(newStudentRow);
      let newStudentNameCol = document.createElement("td");
      let newStudentIdCol = document.createElement("td");
      let newStudentMarkCol = document.createElement("td");
-     let nodeName = document.createTextNode(task[0]);
-     let nodeId = document.createTextNode(task[1]);
-     let nodeMark = document.createTextNode(task[2]);
+     //console.log(studentInfo[0]);
+     let nodeName = document.createTextNode(studentInfo[1]);
+     let nodeId = document.createTextNode(studentInfo[0]);
+     let nodeMark = document.createTextNode(studentInfo[2]);
      newStudentRow.appendChild(newStudentNameCol);
      newStudentNameCol.appendChild(nodeName);
      newStudentRow.appendChild(newStudentIdCol);
-     newStudentNameCol.appendChild(nodeId);
+     newStudentIdCol.appendChild(nodeId);
      newStudentRow.appendChild(newStudentMarkCol);
-     newStudentNameCol.appendChild(nodeMark);
+     newStudentMarkCol.appendChild(nodeMark);    
  }
 
  function addStudent() {
-     let newStudent: String[][] = [
-         ["Student 1", "1", "100"]
-     ];
-     let myJSON = JSON.stringify(newStudent);
-     localStorage.setItem("savedTask", myJSON);
+     let newStudentName: string = (< HTMLInputElement >document.getElementById("studentName")).value;
+     let studentMark: string = (< HTMLInputElement > document.getElementById("studentMark")).value;
+
+     if (localStorage.getItem("savedStudent") === null) {
+         var newStudent = [
+             [parseInt(localStorage.studentID), newStudentName, studentMark]
+         ];
+         var myJSON = JSON.stringify(newStudent);
+         localStorage.setItem("savedStudent", myJSON);
+         var tempID = parseInt(localStorage.studentID);
+         tempID++;
+         localStorage.setItem("studentID", tempID.toString());
+     } else {
+         var tempID = parseInt(localStorage.studentID);
+         var oldTask = JSON.parse(localStorage.savedStudent);
+         oldTask.push([tempID, newStudentName, studentMark]);
+         var myJSON = JSON.stringify(oldTask);
+         localStorage.setItem("savedStudent", myJSON);
+         tempID++;
+         localStorage.setItem("studentID", tempID.toString());
+     }
+     location.reload();
  }
